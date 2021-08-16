9d24c653b78ce6f37dd939afbdd25b002fa8e880
Zack <trimorphyt@gmail.com>
2021-08-16 19:22:24 -0400

    Add diagnostic messages to the parser struct

--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -1,5 +1,6 @@
 //! A hand-written parser that emits an AST tree.
 
+use codespan_reporting::diagnostic::Diagnostic;
 use flycatcher_lexer::{Lexer, Logos, Token};
 
 /// A Parser struct that takes an input string, tokenizes it and parses it into a more or less
@@ -14,6 +15,10 @@ pub struct Parser<'a> {
     /// The string of Flycatcher input that is tokenized and parsed by the parser.  The source
     /// is also used to emit code snippets in diagnostic messages.
     pub source: &'a str,
+
+    /// A list of diagnostics that were created during parsing.  These are not logged to the
+    /// console by the parser, so they can be used to recieve information for IDEs and such.
+    pub diagnostics: Vec<Diagnostic<()>>,
 
     /// The lexer that the parser recieves input tokens from.
     lexer: Lexer<'a, Token>,
