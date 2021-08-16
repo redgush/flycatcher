45f6c0c0ae90ac6f60e4e502563d4edcb31fb573
Zack <trimorphyt@gmail.com>
2021-08-16 18:21:16 -0400

    Convert to a Logos lexer

--- lexer/src/lib.rs
+++ lexer/src/lib.rs
@@ -1,118 +1,72 @@
-//! An efficient hand-written lexer that tokenizes any valid or invalid Flycatcher source.
+//! An efficient lexer for Flycatcher source powered by Logos.
 
-/// A list of tokens that the lexer may output given an input string.
+pub use logos::{Lexer, Logos};
+
+/// A list of tokens that may be matched by the lexer.  Because of the wonderful Logos crate,
+/// this enum also acts as a lexer, for example:
+/// 
+/// ```
+/// // The `Logos` trait is required to use the lexer.
+/// use flycatcher_lexer::Logos;
+/// use flycatcher_lexer::Token;
+/// 
+/// let mut lexer = Token::lexer("'Hello, world!'");
+/// assert_eq!(lexer.next(), Some(Token::String));
+/// assert_eq!(lexer.slice(), "'Hello, world!'");
+/// ```
+#[derive(Clone, Debug, Logos, PartialEq)]
 pub enum Token {
-    
-    /// Matches any character that doesn't match any other token types.  This is recognized as
-    /// an error at parsing time and will always throw an error.  The only time this error isn't
-    /// thrown is in a string, any character may be matched in a string.
+
+    /// A Flycatcher style string literal, which may start and end with either `'` or `"`.  It
+    /// allows escaping characters, but those are not parsed here.
+    /// 
+    /// ```
+    /// use flycatcher_lexer::Logos;
+    /// use flycatcher_lexer::Token;
+    /// 
+    /// let mut lexer = Token::lexer("'Hello, world!' \"Hello, world!\"");
+    /// assert_eq!(lexer.next(), Some(Token::String));
+    /// assert_eq!(lexer.slice(), "'Hello, world!'");
+    /// assert_eq!(lexer.next(), Some(Token::String));
+    /// assert_eq!(lexer.slice(), "\"Hello, world!\"");
+    /// ```
+    #[regex("\"([^\"\\\\]*(\\.[^\"\\\\]*)*)\"|'([^'\\\\]*(\\.[^'\\\\]*)*)'")]
+    String,
+
+    /// A Flycatcher style identifier literal.  An identifier must start with one of
+    /// `a-z`/`A-Z`, `_` or `$`.  Any character after that must be one of `a-z`/`A-Z`, `_`, `$`
+    /// or `0-9`.
+    /// 
+    /// ```
+    /// use flycatcher_lexer::Logos;
+    /// use flycatcher_lexer::Token;
+    /// 
+    /// let mut lexer = Token::lexer("Hello");
+    /// assert_eq!(lexer.next(), Some(Token::Identifier));
+    /// assert_eq!(lexer.slice(), "Hello");
+    /// ```
+    #[regex(r"[a-zA-Z_$][a-zA-Z0-9]*")]
+    Identifier,
+
+    /// This token matches any whitespace character, including regular whitespaces, tabs and
+    /// line breaks/new lines.  It is ignored at lexing time and Logos will pass over it if it
+    /// is found.
+    /// 
+    /// ```
+    /// use flycatcher_lexer::Logos;
+    /// use flycatcher_lexer::Token;
+    /// 
+    /// let mut lexer = Token::lexer(" \t\n");
+    /// assert_eq!(lexer.next(), None);
+    /// ```
+    #[regex(r"\s", logos::skip)]
+    Whitespace,
+
+    /// The `Invalid` token matches any character that doesn't match any other token types, it's
+    /// basically a catchall.  This is recognized as an error at parsing time and will always
+    /// throw an error.  The only time that it's impossible to find this token is inside of a
+    /// string, any characters may be matched in a string, provided they are UTF characters.
+    #[error]
     Invalid,
-
-    /// This is returned if the current index of the lexer is greater than or equal to the
-    /// length of the input string, which means the token is an EndOfFile token.
-    EndOfFile,
-
-}
-
-/// A Flycatcher lexer that takes an input string and tokenizes it.
-pub struct Lexer<'a> {
-
-    /// The input (source) string of characters that the lexer reads tokens from.
-    pub source: &'a str,
-
-    /// The index in the string of which the current token in the lexer starts.
-    pub start: usize,
-
-    /// The index in the input string where the current token ends.
-    pub end: usize,
-
-    /// The current token, taken out of the input string, if any.  If there is not a current
-    /// token, then this is merely an empty string of characters.
-    pub slice: &'a str,
-
-    /// The current index in the `source` string that the lexer is reading tokens from.  This is
-    /// advanced by the `next()` method.
-    index: usize,
-
-}
-
-/// The functionality for the lexer.
-impl<'a> Lexer<'a> {
-
-    /// Allocates a lexer that will tokenize the provided string of characters.  This does not
-    /// tokenize any tokens, it merely initializes a Lexer struct and returns it.
-    pub fn new(source: &'a str) -> Self {
-        Self {
-            source,
-            start: 0,
-            end: 0,
-            slice: "",
-            index: 0,
-        }
-    }
-
-    /// Returns whether or not the character provided counts as an alphabetical character.
-    pub fn is_alpha(c: char) -> bool {
-        match c {
-            'a'..='z' |
-            'A'..='Z' => true,
-            _ => false
-        }
-    }
-
-    /// Returns whether or not the provided character is part of the identifier start character
-    /// set.
-    /// 
-    /// - `a-z`
-    /// - `A-Z`
-    /// - `_`
-    /// - `$`
-    pub fn is_iden_start(c: char) -> bool {
-        match c {
-            'a'..='z' |
-            'A'..='Z' |
-            '_' |
-            '$' => true,
-            _ => false
-        }
-    }
-
-    /// Returns whether or not the provided character is part of the identifier continue
-    /// character set.
-    /// 
-    /// - `a-z`
-    /// - `A-Z`
-    /// - `0-9`
-    /// - `_`
-    /// - `$`
-    pub fn is_iden_continue(c: char) -> bool {
-        match c {
-            'a'..='z' |
-            'A'..='Z' |
-            '0'..='9' |
-            '_' |
-            '$' => true,
-            _ => false,
-        }
-    }
-
-    /// Returns whether or not a character counts as a numerical digit.
-    pub fn is_digit(c: char) -> bool {
-        match c {
-            '0'..='9' => true,
-            _ => false,
-        }
-    }
-
-    /// Reads a token from current index of this lexer, returning what type of token was found.
-    /// Advances the lexer's current position to the position of the next token.
-    pub fn next(&mut self) -> Token {
-        if self.index >= self.source.len() {
-            // There is no input string left to read, so we need to return the `EndOfFile`
-            // token.
-            return Token::EndOfFile;
-        }
-        return Token::EndOfFile;
-    }
 
 }
