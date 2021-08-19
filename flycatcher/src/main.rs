8899c912a53ecc44c87b194624f13e8646433cee
Zack <trimorphyt@gmail.com>
2021-08-19 05:06:28 -0400

    Use Cranelift backend with compiler.

--- flycatcher/src/main.rs
+++ flycatcher/src/main.rs
@@ -1,8 +1,10 @@
 use clap::{App, Arg};
 use flycatcherc::FlycatcherFrontend;
+use flycatcherc_clif::{CraneliftBackend, Triple};
 use flycatcher_diagnostic::DiagnosticEmitter;
 use flycatcher_parser::{Parser};
 use std::fs::read_to_string;
+use std::str::FromStr;
 
 fn main() {
     let matches = App::new("Flycatcher")
@@ -25,12 +27,23 @@ fn main() {
                 let mut c = FlycatcherFrontend::new(input, &i);
                 c.convert(ast);
 
-                dbg!(c.hir);
+                //dbg!(c.hir);
 
                 let emitter = DiagnosticEmitter::new(input, &i);
                 emitter.emit(p.diagnostics);
                 let emitter = DiagnosticEmitter::new(input, &i);
-                emitter.emit(c.diagnostics);
+                emitter.emit(c.diagnostics.clone());
+
+                if c.successful() {
+                    // Compile into an object.
+                    let path = std::path::Path::new(input).file_stem().unwrap().to_str().unwrap();
+                    
+                    let mut backend = CraneliftBackend::new(
+                        Triple::from_str("x86_64-pc-unknown-gnu-coff").unwrap(),
+                        path.to_string() + ".o"
+                    );
+                    backend.compile(c);
+                }
             },
             Err(_) => {
                 let emitter = DiagnosticEmitter::new(input, &i);
