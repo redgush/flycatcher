adfc95c81d1757afcd43088e30819d5d73fc1d59
Zack <trimorphyt@gmail.com>
2021-08-23 11:37:57 -0400

    Add useful methods to AstMeta's implementation.

--- ast/src/meta.rs
+++ ast/src/meta.rs
@@ -4,6 +4,7 @@ use std::ops::Range;
 /// Provides "metadata" about an AST item.  This includes where the AST item was found in a source file,
 /// the document comments before it, whether there's a semicolon after it, and so on.
 pub struct AstMeta {
+
     /// The absolute indexes where the AST item starts and ends in the source file.
     pub range: Range<usize>,
 
@@ -15,9 +16,12 @@ pub struct AstMeta {
 
     /// The AST item that this struct wraps.
     pub item: Ast,
+
 }
 
 impl AstMeta {
+
+    /// Creates an AST metadata struct.  `semicolon` defaults to `false`, which may be changed later.
     pub fn new(range: Range<usize>, item: Ast) -> Self {
         Self {
             range,
@@ -26,4 +30,28 @@ impl AstMeta {
             item,
         }
     }
+
+    /// Creates an AST metadata object and immediately boxes it.
+    pub fn boxed(range: Range<usize>, item: Ast) -> Box<Self> {
+        Box::new(
+            Self {
+                range,
+                semicolon: false,
+                comments: vec![],
+                item,
+            }
+        )
+    }
+
+    /// Sets the list of comments for this AST item.
+    pub fn with_comments(mut self, comments: Vec<String>) -> Self {
+        self.comments = comments;
+        self
+    }
+
+    /// Sets the value of `semicolon` to `true`.
+    pub fn semicolon(&mut self) {
+        self.semicolon = true;
+    }
+
 }
