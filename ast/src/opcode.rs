c3f6b61018590d87837196459827dde671120651
Zack <trimorphyt@gmail.com>
2021-08-23 12:26:50 -0400

    Implement useful AST items.

--- ast/src/opcode.rs
+++ ast/src/opcode.rs
@@ -1,16 +1,28 @@
+use flycatcher_lexer::Token;
+
 /// A list of opcodes that may be used in binary expressions.
 #[derive(Clone, Copy, Debug, PartialEq)]
-pub enum BinaryOpcode {
+pub enum Opcode {
+    /// A member access using the `.` operator.
+    MemberAccess,
+
+    /// A member access using the `[]` operators.
+    Subscript,
+
+    Comma,
     GreaterGreater,
     LessLess,
     EqualsEquals,
     ExclamationEquals,
     GreaterEquals,
     LessEquals,
+    AndAnd,
+    OrOr,
     Greater,
     Less,
     And,
     Or,
+    Caret,
     Not,
     Equals,
     Plus,
@@ -18,4 +30,61 @@ pub enum BinaryOpcode {
     Asterisk,
     Slash,
     Percent,
+}
+
+impl Opcode {
+    /// Returns the opcode associated with a token, if any.
+    pub fn from_token(tok: Token) -> Option<Opcode> {
+        match tok {
+            Token::Period => Some(Opcode::MemberAccess),
+            Token::LBrack => Some(Opcode::Subscript),
+            Token::Comma => Some(Opcode::Comma),
+            Token::GreaterGreater => Some(Opcode::GreaterGreater),
+            Token::LessLess => Some(Opcode::LessLess),
+            Token::EqualsEquals => Some(Opcode::EqualsEquals),
+            Token::ExclamationEquals => Some(Opcode::ExclamationEquals),
+            Token::GreaterEquals => Some(Opcode::GreaterEquals),
+            Token::LessEquals => Some(Opcode::LessEquals),
+            Token::AndAnd => Some(Opcode::AndAnd),
+            Token::OrOr => Some(Opcode::OrOr),
+            Token::Caret => Some(Opcode::Caret),
+            Token::Not => Some(Opcode::Not),
+            Token::Equals => Some(Opcode::Equals),
+            Token::Plus => Some(Opcode::Plus),
+            Token::Minus => Some(Opcode::Minus),
+            Token::Asterisk => Some(Opcode::Asterisk),
+            Token::Slash => Some(Opcode::Slash),
+            Token::Percent => Some(Opcode::Percent),
+            _ => None,
+        }
+    }
+
+    /// Calculates the precedence of this binary operator.
+    pub fn precedence(&self) -> usize {
+        match self {
+            Self::MemberAccess => 100,
+            Self::Subscript => 100,
+            Self::Not => 99,
+            Self::Asterisk => 98,
+            Self::Slash => 98,
+            Self::Percent => 98,
+            Self::Plus => 97,
+            Self::Minus => 97,
+            Self::GreaterGreater => 96,
+            Self::LessLess => 96,
+            Self::Greater => 95,
+            Self::Less => 95,
+            Self::GreaterEquals => 95,
+            Self::LessEquals => 95,
+            Self::EqualsEquals => 94,
+            Self::ExclamationEquals => 94,
+            Self::And => 93,
+            Self::Caret => 92,
+            Self::Or => 91,
+            Self::AndAnd => 90,
+            Self::OrOr => 89,
+            Self::Equals => 88,
+            Self::Comma => 87,
+        }
+    }
 }
