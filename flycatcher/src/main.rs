b84d9399b11b6c58ac523d9ffc97a8113fff0c3f
Zack <trimorphyt@gmail.com>
2021-08-27 21:07:25 -0400

    Add timer/benchmark.

--- flycatcher/src/main.rs
+++ flycatcher/src/main.rs
@@ -8,7 +8,14 @@ fn main() {
     {
         let mut tmp_ctx = ctx.clone();
         let mut parser = Parser::new(&mut tmp_ctx);
-        dbg!(parser.parse_expression());
+
+        let start = std::time::Instant::now();
+        let ast = parser.parse();
+        let end = start.elapsed().as_nanos();
+
+        dbg!(ast);
         parser.context.emit();
+
+        println!("Parsed in {}ms", end as f64 / 1e+6)
     }
 }
