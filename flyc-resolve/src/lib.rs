5fbae0a9d4378dbe3c74820ce986d08c89eaf945
Zack <trimorphyt@gmail.com>
2021-08-31 17:11:53 -0400

    Fix module resolving bug

--- flyc-resolve/src/lib.rs
+++ flyc-resolve/src/lib.rs
@@ -18,7 +18,7 @@ pub fn get_debug_name(mut abs: String) -> PathBuf {
 /// Gets the absolute path to a Flycatcher module.  It will check the `src` path first because
 /// it has higher priority, then it will check the modules path.
 pub fn resolve_path(name: String, src: String) -> Option<PathBuf> {
-    let mut tmp_name = name;
+    let mut tmp_name = name.clone();
 
     if !tmp_name.ends_with(".flyc") {
         tmp_name.push_str(".flyc");
@@ -32,7 +32,7 @@ pub fn resolve_path(name: String, src: String) -> Option<PathBuf> {
         exe.pop();
 
         let mod_path = exe.join("lib");
-        let folder = mod_path.join(Path::new(&src));
+        let folder = mod_path.join(&name);
 
         if folder.exists() {
             let module_file = folder.join(Path::new("mod.flyc"));
